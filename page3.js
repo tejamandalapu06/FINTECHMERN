d67723f5a51a03227550fa5380d7d4a5146a69eb
tejamandalapu06 <teja.mandalapu06@gmail.com>
2026-03-05 12:08:40 +0530

    Created page3.js

---
+++ page3.js
@@ -0,0 +1,21 @@
+//Primitive
+let a  = 10 //Number
+let name = "Geetika" //String
+let flag = true //Boolean
+let promoted = undefined //Undefined
+let section = null //Null type
+let num = 654757756858586 // BigInt
+
+//Non-Primitive - Reference Type
+const student = {
+    name:"John",
+    age:21
+}
+console.log(student)
+const s1 = student
+console.log(s1)
+student.age = 25
+console.log(s1)
+
+//Arrays
+//Functions
