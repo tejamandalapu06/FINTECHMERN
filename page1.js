54c2eefedf97a88679bc31ef2fd2ec5f2d83ee9b
tejamandalapu06 <teja.mandalapu06@gmail.com>
2026-03-05 12:06:22 +0530

    Created  page1.js

---
+++ page1.js
@@ -0,0 +1,34 @@
+// //let, var, const
+// let a = 10
+// console.log(a)
+// //let a=11
+// //console.log(a)
+
+
+// var b = 20
+// console.log(b)
+// var b=21
+// console.log(b)
+
+// const c =30
+// console.log(c)
+//c=31
+//console.log(c)
+
+//hoisting
+// console.log(a)
+// var a = 10
+
+// var a
+// console.log(a)
+// a = 10
+// console.log(a)
+
+console.log(a)
+let a=10   //TDZ - Temporal Dead Zone
+
+// let a
+// console.log(a)
+// a=10
+
+//same applies to const as well
